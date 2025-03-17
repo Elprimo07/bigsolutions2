71a421527fe075e46b9ffe5402470069644544a5
Elprimo07 <92458348+Elprimo07@users.noreply.github.com>
2025-03-17 08:47:06 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Gracias por contactar a Big Solutions. Nos pondremos en contacto contigo pronto.');
+    // Aquí puedes agregar la lógica para enviar el formulario a un servidor
+});
